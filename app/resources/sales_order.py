1a0023c1b5b781963730cb71808ee4004b029187
ABHISHEK SHARMA <abhisheksharma@ABHISHEKs-MacBook-Air.local>
2023-04-17 00:15:29 +0530

    changing sales order api format

--- app/resources/sales_order.py
+++ app/resources/sales_order.py
@@ -29,7 +29,7 @@ order_dummy_data = [
                     "order_item_id" : "2"
                 } 
             ],
-            "total_kegs" : "20",
+            "total_kegs" : "10",
         },
     ]
     }
@@ -40,22 +40,24 @@ order_dummy_data = [
         "status" : "success",
         "data" : [
         {
-            "draught_code" : "PINA618",
-            "outlet_name" : "Gold Wings Hotel",
+            "draught_code" : "SSAE358",
+            "outlet_name" : "MJ Bar",
             "area" : "kolara",
             "kegs_data" : [
                 {
                     "keg_beer_name" : "BUD",
                     "keg_beer_capacity" : "30",
-                    "keg_quantity" : "6"
+                    "keg_quantity" : "12",
+                    "order_item_id" : "1"
                 },
                 {
-                    "keg_beer_name" : "HOG",
-                    "keg_beer_capacity" : "15",
-                    "keg_quantity" : "4"
+                    "keg_beer_name" : "MAG",
+                    "keg_beer_capacity" : "30",
+                    "keg_quantity" : "18",
+                    "order_item_id" : "2"
                 } 
             ],
-            "total_kegs" : "20",
+            "total_kegs" : "30",
         },
     ]
     }
@@ -66,9 +68,9 @@ order_dummy_data = [
 # shows a list of all the sales order.
 class SalesOrderListResource(Resource):
     # date should be in "YYYY-MM-DD" format
-    def get(self, order_date=""):
-        if order_date is None:
-            return {"message": "No order data provided"}, 400
+    def get(self):
+        if not request.args.get('order_date'):
+            return dict(status="failed",message= "No order date provided"), 400
         else:
             so_numbers = [dict(so_numbers="QDSON"+ str(i), so_id=str(i)) for i in range(0,10)]
             return {"so_numbers": so_numbers}, 200
@@ -76,13 +78,9 @@ class SalesOrderListResource(Resource):
 
 class SalesOrderResource(Resource):
     def get(self, order_id = None):
-        data = request.get_json()
-        if not data:
-            return {"message": "No input data provided"}, 400
-        if not data.get("order_id"):
-            return {"message": "No order id provided"}, 400
-        order_id = data.get("order_id")
-        # print(order_id)
+        if not request.args.get('order_id'):
+            return dict(status="failed",message= "No order id provided"), 400
+        order_id = request.args.get('order_id')
         for so in order_dummy_data:
             if so['id'] == order_id:
                 return dict(status=200,order_data=so['data']), 200
